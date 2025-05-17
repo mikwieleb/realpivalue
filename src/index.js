780d3659332ebba57f1d95401d4a9824dd2ec68f
mikwieleb <mik.wieleb@gmail.com>
2025-05-17 02:46:18 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 
+// Crée le point d'ancrage React dans l'élément root de index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
