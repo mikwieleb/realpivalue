8dc794aaad0637314a651eeff3f59ec8a1f49f82
mikwieleb <mik.wieleb@gmail.com>
2025-05-19 21:04:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
+// src/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './pi-sdk';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
