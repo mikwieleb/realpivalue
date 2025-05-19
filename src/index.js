5639e063fb9d7e8fab7b9dd38957288bb809f87c
mikwieleb <mik.wieleb@gmail.com>
2025-05-19 02:17:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-// Activer le mode sandbox
-window.__PI_NETWORK_SANDBOX__ = true;
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
