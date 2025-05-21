b8db2ed5ae5fd7f4f69cc7718142b1df3fae30cb
mikwieleb <mik.wieleb@gmail.com>
2025-05-21 22:03:00 +0200

    Update pi-sdk.js

--- src/pi-sdk.js
+++ src/pi-sdk.js
@@ -1,7 +1,7 @@
 // src/pi-sdk.js
 
 if (typeof window !== 'undefined') {
-  // Active le mode SANDBOX avant de charger le SDK
+  // Active le mode sandbox pour le testnet
   window.__PI_NETWORK_SANDBOX__ = true;
 
   const isPiBrowser = window?.navigator?.userAgent.includes('PiBrowser');
@@ -9,9 +9,6 @@ if (typeof window !== 'undefined') {
     const script = document.createElement('script');
     script.src = 'https://sdk.minepi.com/pi-sdk.js';
     script.async = true;
-    script.onload = () => {
-      console.log('Pi SDK loaded');
-    };
     document.head.appendChild(script);
   }
 }
